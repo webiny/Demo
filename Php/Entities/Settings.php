4a75179e0181be6d59218def8bc518aac0b4ad6a
Pavel910 <pavel@webiny.com>
2017-09-28 03:26:04 +0200

    Updates on notifications and settings classes.

--- Php/Entities/Settings.php
+++ Php/Entities/Settings.php
@@ -4,7 +4,7 @@ namespace Apps\Demo\Php\Entities;
 /**
  * Class Settings
  */
-class Settings extends \Apps\Webiny\Php\Entities\Settings
+class Settings extends \Apps\Webiny\Php\Entities\AbstractSettings
 {
     protected static $classId = 'Demo.Entities.Settings';
     protected static $key = 'demo';
